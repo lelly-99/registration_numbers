86780ad1b7884f907472293b44dd58c0b3488850
progress <leswifil@gmail.com>
2023-07-18 13:20:01 +0200

    dynamically added a div

--- registration/registration_numbers.js
+++ registration/registration_numbers.js
@@ -1,1 +1,10 @@
 //references
+const addButton = document.querySelector(".addButton")
+const registationPlates = document.querySelector(".registationPlates")
+
+
+addButton.addEventListener('click', function () {
+    const newDiv = document.createElement("div")
+    newDiv.classList.add("plate")
+    registationPlates.appendChild(newDiv)
+  });
